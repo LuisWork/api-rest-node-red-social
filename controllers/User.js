68f58b443ad72e6eed3d5861da31e428b8c9cfe8
Luis Zambrano <LuisZambrano.Work@gmail.com>
2023-11-27 17:21:51 -0300

    Creando controladores y metodos de prueba REST

--- controllers/User.js
+++ controllers/User.js
@@ -1,1 +1,10 @@
 // Acciones de prueba
+const pruebaUser = (req, res) => {
+    return res.status(200).send({
+        message: "Mensaje enviado desde el controlador: controllers/User.js"
+    })
+};
+
+module.exports = {
+    pruebaUser
+}
