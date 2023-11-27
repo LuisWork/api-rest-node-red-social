f2f80c6061d2975d859ac228d81fc1295f40e9a9
Luis Zambrano <LuisZambrano.Work@gmail.com>
2023-11-27 17:08:23 -0300

    Implementando conexion a la base de datos de MongoDB

---
+++ database/connection.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connection = async() => {
+
+    try{
+        await mongoose.connect("mongodb://127.0.0.1:27017/mi_red_social");
+        console.log("Conectado a la base de datos correctamente")
+    }catch(error){
+        console.log(error);
+        throw new Error("No se ha podido conectar a la base de datos");
+    }
+
+}
+
+module.exports = {
+    connection
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+// Importar dependencias
+const { connection } = require("./database/connection");
+
+// Mensaje de bienvenida
+console.log("API NODE para Red Social Inicializada");
+
+// Conexion a la BBDD
+
+connection();
+
+// Crear servidor de Node
+
+// Configurar CORS
+
+// Convertir los datos del body a objetos JSON
+
+// Cargar la configuracion de rutas
+
+// Poner servidor a escuchar peticiones HTTP
