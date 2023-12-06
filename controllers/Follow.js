5217fe88da3d4e88e01a5a1f409bcd06f2cd878d
Luis Zambrano <LuisZambrano.Work@gmail.com>
2023-12-06 11:51:15 -0300

    Implementando modelo y controlador de follow

--- controllers/Follow.js
+++ controllers/Follow.js
@@ -1,10 +1,21 @@
+const follow = require("../models/follow");
+const user = require("../models/user");
+
 // Acciones de prueba
 const pruebaFollow = (req, res) => {
-    return res.status(200).send({
-        message: "Mensaje enviado desde el controlador: controllers/follow.js"
-    })
+  return res.status(200).send({
+    message: "Mensaje enviado desde el controlador: controllers/follow.js",
+  });
 };
 
+// Accion de seguir
+
+// Accion de dejar de seguir
+
+// Listado de usuarios que estoy siguiendo
+
+// Accion de listar usuarios que me siguen
+
 module.exports = {
-    pruebaFollow
-}
+  pruebaFollow,
+};
